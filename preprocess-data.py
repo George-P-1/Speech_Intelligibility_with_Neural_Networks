e2ca7c035b38a52e2eea0f59c96c7b08e02226dc
George <George-P-1@users.noreply.github.com>
2025-02-05 15:28:50 +0100

    renamed and added empty scripts

--- preprocess-data.py
+++ preprocess-data.py
@@ -7,7 +7,6 @@ import soundfile as sf
 
 
 # SECTION Main code here
-
 @hydra.main(version_base=None, config_path=".", config_name="config")
 def main(path_data: DictConfig) -> None:
     # print(path_data.test_path.ref_file) # REMOVE_LATER
@@ -16,7 +15,6 @@ def main(path_data: DictConfig) -> None:
         with open(path_data.test_path.ref_file, 'r') as ref_file:
             ref_json = json.load(ref_file)
             
-            # TODO - Make a automated way to open each spin and target anechoic scene using loop. Or maybe this script just needs to work on one prompt on each run.
             print(ref_json[0], "\n")    # REMOVE_LATER
             # TODO - NOTE - Path of audio files to open, HA_Output and target_anechoic
             spin_file_path = Path(path_data.test_path.spin_folder) / f"{ref_json[0]['signal']}.wav"
@@ -32,9 +30,6 @@ def main(path_data: DictConfig) -> None:
         return None
     finally:
         print(f'Finished processing JSON file.')
-
-
-
 # !SECTION
 
 if __name__ == '__main__':
