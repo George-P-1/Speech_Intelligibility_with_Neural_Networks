8e23d719cb9939b6f6f9807c414e3e991b04100f
George <George-P-1@users.noreply.github.com>
2025-02-05 16:19:56 +0100

    made few changes to align with test data

--- preprocess-data.py
+++ preprocess-data.py
@@ -7,24 +7,21 @@ import json
 # SECTION Main code here
 @hydra.main(version_base=None, config_path=".", config_name="config")
 def main(path_data: DictConfig) -> None:
-    # print(path_data.test_path.ref_file) # REMOVE_LATER
-    # Open reference JSON file
+    print(path_data.train_path.ref_file) # REMOVE_LATER
+    
+    # Open training reference JSON file
     try:
-        with open(path_data.test_path.ref_file, 'r') as ref_file:
-            ref_json = json.load(ref_file)
-            
-            print(ref_json[0], "\n")    # REMOVE_LATER
-            # TODO - NOTE - Path of audio files to open, HA_Output and target_anechoic
-            spin_file_path = Path(path_data.test_path.spin_folder) / f"{ref_json[0]['signal']}.wav"
-            print(spin_file_path, "\n")  # REMOVE_LATER
-            target_file_path = Path(path_data.test_path.scenes_folder) / f"{ref_json[0]['scene']}_target_anechoic.wav"
-            print(target_file_path, "\n")  # REMOVE_LATER
-            # TODO - NOTE - Opening audio files using soundfile
-            pass
+        with open(path_data.train_path.ref_file, 'r') as ref_file:
+            ref_json = json.load(ref_file)   # Load the JSON file
+
+            print(f'The loaded json data is of type: {type(ref_json)}'); print(f"Number of items in the JSON array: {len(ref_json)}"); print(ref_json[0], "\n")    # REMOVE_LATER
+            # NOTE - Path of audio files to open, HA_Output and target_anechoic
+            spin_file_path = Path(path_data.train_path.spin_folder) / f"{ref_json[0]['signal']}.wav"; print(spin_file_path, "\n")  # REMOVE_LATER
+            target_file_path = Path(path_data.train_path.scenes_folder) / f"{ref_json[0]['scene']}_target_anechoic.wav"; print(target_file_path, "\n")  # REMOVE_LATER
 
         ref_file.close()
     except FileNotFoundError:
-        print(f'File not found: {path_data.test_path.ref_file}')
+        print(f'File not found: {path_data.train_path.ref_file}')
         return None
     finally:
         print(f'Finished processing JSON file.')
